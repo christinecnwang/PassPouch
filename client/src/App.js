45cc185a0db977872394b8cbb1e8825442755ec9
christinecnwang <christinecnwang@gmail.com>
2024-06-09 17:00:16 -0400

    Updated styling

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import React from "react";
 import { useState, useEffect } from "react";
 import Axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   const [app, setApp] = useState("");
   const [password, setPassword] = useState("");
   const [passwordList, setPasswordList] = useState([]);
+  const [successMessage, setSuccessMessage] = useState("");
 
   useEffect(() => {
     Axios.get("http://localhost:3001/getpasswords").then((response) => {
@@ -23,6 +26,9 @@ function App() {
   const addPassword = (event) => {
     event.preventDefault();
 
+    setApp("");
+    setPassword("");
+
     Axios.post("http://localhost:3001/addpassword", {
       app: app,
       password: password,
@@ -35,8 +41,8 @@ function App() {
             text: item.app,
           }))
         );
-        setApp("");
-        setPassword("");
+        setSuccessMessage("Success!");
+        setTimeout(() => setSuccessMessage(""), 2000);
       });
     });
   };
@@ -84,37 +90,66 @@ function App() {
 
   return (
     <div className='App'>
-      <form className='AddPassword' onSubmit={addPassword}>
-        <input
-          type='text'
-          placeholder='Enter app name'
-          onChange={(event) => setApp(event.target.value)}
-          required
-        />
-        <input
-          type='text'
-          placeholder='Enter password'
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          required
-        />
-        <button type='submit'>Add Password</button>
-      </form>
+      <div className='Banner'>
+        <div className='Title'>PassPouch</div>
+      </div>
 
-      <div className='Passwords'>
-        {passwordList.map((val, key) => {
-          return (
-            <div
-              className='Password'
-              onClick={() => {
-                togglePassword(val);
-              }}
-              key={key}
-            >
-              <h3>{val.text}</h3>
+      <div className='MainContent'>
+        <div className='LeftPanel'>
+          <div className='Intro'>
+            <img
+              src={`${process.env.PUBLIC_URL}/kangaroo-cartoon.png`}
+              alt='Kangaroo'
+              className='Kangaroo'
+            />
+            <div className='IntroText'>
+              <h1>Hello there!</h1>
+              <h2>Add your passwords to my pouch, and I'll keep them safe!</h2>
+              <h2>
+                View your saved passwords on the right & click the eye icon to
+                toggle visibility.
+              </h2>
             </div>
-          );
-        })}
+          </div>
+
+          <form className='AddPassword' onSubmit={addPassword}>
+            <input
+              type='text'
+              placeholder='Enter app name'
+              value={app}
+              onChange={(event) => setApp(event.target.value)}
+              required
+            />
+            <input
+              type='text'
+              placeholder='Enter password'
+              value={password}
+              onChange={(event) => setPassword(event.target.value)}
+              required
+            />
+            <button type='submit'>
+              {successMessage ? "Success!" : "Add Password"}
+            </button>
+          </form>
+        </div>
+
+        <div className='Passwords'>
+          <h2>Your Saved Passwords:</h2>
+          {passwordList.map((val, key) => {
+            return (
+              <div className='PasswordRow'>
+                <div className='Password' key={key}>
+                  <h3>{val.text}</h3>
+                </div>
+                <FontAwesomeIcon
+                  className='EyeIcon'
+                  icon={faEye}
+                  onClick={() => togglePassword(val)}
+                />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
